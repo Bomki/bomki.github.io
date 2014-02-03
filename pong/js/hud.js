107400671a3c0f2f6245630d39d453c099eaada9
Bomki <roehrig.marcel@gmail.com>
2014-02-03 01:01:45 +0100

    Hud template

---
+++ pong/js/hud.js
@@ -0,0 +1,18 @@
+function Hud(game, x, y){
+    
+    this.x = x;
+    this.y = y;
+    
+    this.preload = function(){
+        
+    };
+    
+    this.create = function(){
+        
+    };
+    
+    this.update = function(){
+        
+    };
+    
+}
