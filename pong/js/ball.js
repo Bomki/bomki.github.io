562ba28d7df4eaa8bab55ed47a97fb1b000471b3
Bomki <roehrig.marcel@gmail.com>
2014-02-04 23:50:56 +0100

    test

--- pong/js/ball.js
+++ pong/js/ball.js
@@ -1,4 +1,4 @@
-function Ball (game, spriteName, spriteLocation, x, y, startSpeedX, startSpeedY, accX, accY){
+function Ball (game, spriteName, spriteLocation, x, y, startSpeedX, startSpeedY, accX, accY, timeoutAmount){
     
     this.sprite = Phaser.Sprite;
     this.x = x;
@@ -9,6 +9,9 @@ function Ball (game, spriteName, spriteLocation, x, y, startSpeedX, startSpeedY,
     this.spriteLocation = spriteLocation;
     this.resetBall = function(){
         this.create();
+        
+    this.timeoutAmount = timeoutAmount;
+    this.timeout = null;
     };
     
     this.preload = function () {
@@ -34,26 +37,31 @@ function Ball (game, spriteName, spriteLocation, x, y, startSpeedX, startSpeedY,
     
     this.update = function () {
 
+        if (game.time.time > this.timeout) {
+            timeout = game.time.time + this.timeoutAmount; 
+            
+            if(this.sprite.body.velocity.x > 0){
+                this.sprite.body.velocity.x += this.accX;
+            }
         
+            else if(this.sprite.body.velocity.x < 0){
+                this.sprite.body.velocity.x -= this.accX;
+            }
+        
+            if(this.sprite.body.velocity.y > 0){
+                this.sprite.body.velocity.x += this.accY;
+            }
+        
+            else if(this.sprite.body.velocity.y < 0){
+                this.sprite.body.velocity.x -= this.accY;
+            } 
+            
+        }
 
         
-        if(this.sprite.body.velocity.x > 0){
-            this.sprite.body.velocity.x += this.accX;
-        }
         
-        if(this.sprite.body.velocity.x < 0){
-            this.sprite.body.velocity.x -= this.accX;
-        }
-        
-        if(this.sprite.body.velocity.y > 0){
-            this.sprite.body.velocity.x += this.accY;
-        }
-        
-        if(this.sprite.body.velocity.y < 0){
-            this.sprite.body.velocity.x -= this.accY;
-        }
         
     };
 }
 
-var ball1 = new Ball(game, "ball1", "assets/ball.png", 680, 384, 400, 400, 1, 1);
+var ball1 = new Ball(game, "ball1", "assets/ball.png", 680, 384, 5000, 400, 0, 0, 4000);
